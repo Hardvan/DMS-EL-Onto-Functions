75fafaf3eec236df0bf291066fee830683aa6150
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2023-02-26 14:54:20 +0530

    Update REAMDE.md

--- Code/code.py
+++ Code/code.py
@@ -12,18 +12,18 @@ def calculate_onto_functions(S, T, repeated=False):
     # Calculate the number of onto functions from S to T
     n = len(S)
     m = len(T)
-    num_onto_functions = m ** n
+    num_onto = m ** n
 
     # List all of the onto functions from S to T
-    onto_functions = []
+    onto = []
     if not repeated:
         for t in permutations(T, n):
-            onto_functions.append(dict(zip(S, t)))
+            onto.append(dict(zip(S, t)))
     else:
         for t in product(T, repeat=n):
-            onto_functions.append(dict(zip(S, t)))
+            onto.append(dict(zip(S, t)))
 
-    return num_onto_functions, onto_functions
+    return num_onto, onto
 
 
 # Example Usage
@@ -31,14 +31,14 @@ S = {1, 2, 3}
 T = {'a', 'b', 'c', 'd'}
 
 repeated = input(
-    "Do you want to allow repeated elements in the onto functions? (y/n): ")
+    "Allow repeated elements in the onto functions? (y/n): ")
 repeated = repeated.lower() == 'y'
 
-num_onto_functions, onto_functions = calculate_onto_functions(S, T, repeated)
+num_onto, onto = calculate_onto_functions(S, T, repeated)
 
-print("Number of onto functions from S to T:", num_onto_functions)
+print("Number of onto functions from S to T:", num_onto)
 
 # Display all of the onto functions
 print("All of the onto functions from S to T:")
-for f in onto_functions:
+for f in onto:
     print(f)
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Problem Statement
 
 ### Calculate the number of onto functions from a set S to a set T, where S and T are finite sets of various sizes. List all of them.
+
+## Usage
+
+### The problem statement requires calculating the number and listing all onto functions from S to T, where S and T are finite sets of various sizes. An onto function is a function that maps every element in S to at least one element in T.
+
+## Dependencies
+
+### The code requires the itertools module, which is a built-in Python module.
